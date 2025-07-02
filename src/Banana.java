addef0543d8ff04d7e6c67c77cb178db955b95a3
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-02 11:42:20 +0400

    add:Banana Object Code

---
+++ src/Banana.java
@@ -0,0 +1,14 @@
+public class Banana {
+    boolean iseaten;
+    int length;
+    public void ripen(boolean isripen)
+    {
+        if(isripen)
+        {
+            iseaten = true;
+        }
+        else
+            iseaten = false;
+    }
+
+}
